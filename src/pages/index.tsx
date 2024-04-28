d961115bdd4259c7f08f2d5e0e42b03b7391f664
lazybear-0425 <wendy930425@gmail.com>
2024-04-29 02:42:48 +0800

    新增favicon.ico

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import type { Metadata } from 'next'
+import Head from 'next/head'; // 导入 Head 组件
 import Nav from "./html_tags/nav";
 //import Section from "./html_tags/section";
 import Aside from "./html_tags/aside";
@@ -11,17 +12,14 @@ import Separator from '@/pages/html_tags/article/separator';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: '熊熊快樂小組',
-  description: 'ACM group, named 熊熊快樂小組',
-  icons: {
-    icon: '/favicon.png',
-  },
-};
-
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>熊熊快樂小組</title> {/* 设置页面标题 */}
+        <meta name="description" content='ACM group, named 熊熊快樂小組' /> {/* 设置页面描述 */}
+        <link rel="icon" href='/favicon.ico' sizes="any" /> {/* 设置页面图标 */}
+      </Head>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <Nav />
       <section>
