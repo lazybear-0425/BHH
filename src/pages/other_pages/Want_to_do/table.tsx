5350d312c5c4fade6184f3f88f2ce484906942c4
lazybear-0425 <wendy930425@gmail.com>
2024-05-02 21:30:00 +0800

    update

--- src/pages/other_pages/Want_to_do/table.tsx
+++ src/pages/other_pages/Want_to_do/table.tsx
@@ -74,6 +74,27 @@ export default function Example() {
             )}
             </Disclosure>
         </div>
+        
+        <br />
+        <div style={{width:'70%',marginLeft: '20px'}}>
+            <Disclosure>
+            {({ open }) => (
+                <>
+                <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+                    <span>每週一個C++ function分享</span>
+                    <ChevronUpIcon
+                    className={`${
+                        open ? 'rotate-180 transform' : ''
+                    } h-5 w-5 text-purple-500`}
+                    />
+                </Disclosure.Button>
+                <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-gray-500">
+                    可以先從STL開始<br/>
+                </Disclosure.Panel>
+                </>
+            )}
+            </Disclosure>
+        </div>
         </div>
     </article>
   )
